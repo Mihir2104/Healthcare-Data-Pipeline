b794c40134d566f6c9b2cb4564fc429c8a8450f7
Mihir2104 <63975338+Mihir2104@users.noreply.github.com>
2025-08-19 16:05:57 +0530

    Add files via upload

---
+++ data/configs/audit_table_ddl.sql
@@ -0,0 +1,8 @@
+CREATE TABLE infra-memento-469009-a5.temp_dataset.audit_log (
+    data_source STRING,
+    tablename STRING,
+    load_type STRING,
+    record_count INT64,
+    load_timestamp TIMESTAMP,
+    status STRING
+);
